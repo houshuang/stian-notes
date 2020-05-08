1eeb4ec69c2f8d093135f9120bfd357bab5d06b5
Stian Haklev <stian@minervaproject.com>
2020-05-08 20:04:38 +0200

    WIP

--- src/gatsby-theme-andy/components/BrainNote.js
+++ src/gatsby-theme-andy/components/BrainNote.js
@@ -40,13 +40,13 @@ const BrainNote = ({ note }) => {
     <MDXProvider components={components}>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{note.title} - aravindballa's notes</title>
+        <title>{note.title} - Stian's notes</title>
       </Helmet>
       <div
         id="brainNote"
         className="container max-w-2xl px-4 mx-auto text-gray-900 flex flex-col min-h-screen"
       >
-        <div className="font-bold py-2 border-b -mx-4 px-4">@aravindballa's notes</div>
+        <div className="font-bold py-2 border-b -mx-4 px-4">Stian's notes</div>
         <div className="flex-1">
           <h1 className="my-4">{note.title}</h1>
           <MDXRenderer>{note.childMdx.body}</MDXRenderer>
@@ -55,8 +55,8 @@ const BrainNote = ({ note }) => {
           {referenceBlock}
           <p className="text-sm m-0">
             If you think this note resonated, be it positive or negative, send me a{' '}
-            <a href="https://twitter.com/messages/compose?recipient_id=532906019">direct message</a>{' '}
-            on Twitter or an <a href="mailto:bsaaravind+notes@gmail.com">email</a> and we can talk.
+            <a href="https://twitter.com/houshuang">direct message</a> on Twitter or an{' '}
+            <a href="mailto:shaklev@gmail.com">email</a> and we can talk.
           </p>
         </div>
       </div>
