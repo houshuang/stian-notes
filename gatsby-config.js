c2b7b721856a6c185d38baf7f3ac0a3c6cfe30ae
Stian Haklev <stian@minervaproject.com>
2020-05-17 13:58:53 +0200

    Adding Google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  plugins: ['gatsby-theme-andy', `gatsby-plugin-postcss`],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-101270563-2',
+      },
+    },
+    'gatsby-theme-andy',
+    `gatsby-plugin-postcss`,
+  ],
 };
